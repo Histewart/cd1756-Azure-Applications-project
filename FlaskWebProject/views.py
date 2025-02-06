5141a08d21d1267c877a05c1c9ad4093da69f02b
Histewart <hunterstewart@chevron.com>
2025-02-05 19:11:31 -0600

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,3 +1,7 @@
+"""
+Routes and views for the flask application.
+"""
+
 from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
